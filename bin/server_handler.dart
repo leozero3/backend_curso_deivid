a9a8b468860ef3d0ca169f570dbdbc3914acf21e
Leo Home <realleo2009@hotmail.com>
2023-09-12 22:41:22 -0300

    #6

--- bin/server_handler.dart
+++ bin/server_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
@@ -9,16 +11,28 @@ class ServeHandler {
       return Response(200, body: 'Primeira rota');
     });
 
-    router.get('/ola/mundo/<usuario>', (Request req, String usuario){
+    router.get('/ola/mundo/<usuario>', (Request req, String usuario) {
       return Response.ok("ola mundo $usuario");
     });
 
-
-    router.get('/query', (Request req){  //http://localhost:8080/query?nome=leo&idade=26
+    router.get('/query', (Request req) {
+      //http://localhost:8080/query?nome=leo&idade=26
       String? nome = req.url.queryParameters['nome'];
       String? idade = req.url.queryParameters['idade'];
       return Response.ok('Query: $nome, idade: $idade');
+    });
 
+    router.post('/login', (Request req) async {
+      var result = await req.readAsString();
+      Map json = jsonDecode(result);
+
+      var usuario = json['usuario'];
+      var senha = json['senha'];
+      if (usuario == 'admin' && senha == '123') {
+        return Response.ok('Bem vindo $usuario');
+      } else {
+        return Response.forbidden('Acesso negado');
+      }
     });
 
     return router;
