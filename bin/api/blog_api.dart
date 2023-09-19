5d5f4af9ef42ae9e10a9f68308df0c0e59311db5
leoW <realleo2009@hotmail.com>
2023-09-19 15:07:33 -0300

    #10

--- bin/api/blog_api.dart
+++ bin/api/blog_api.dart
@@ -3,10 +3,29 @@ import 'package:shelf_router/shelf_router.dart';
 
 class BlogApi {
   Handler get handler {
+
     Router router = Router();
-    router.get('/blog/noticias', (Request req) {
-      return Response.ok('sol hoje');
+    router.get('/blog/news', (Request req) {
+      return Response.ok('Choveu hoje');
     });
+
+    router.post('/blog/news', (Request req){
+      return Response.ok('Choveu hoje');
+    });
+
+    router.put('/blog/news', (Request req){
+      String? id = req.url.queryParameters['id'];
+      return Response.ok('Choveu hoje');
+    });
+
+    router.delete('/blog/news', (Request req){
+      String? id = req.url.queryParameters['id'];
+      return Response.ok('Choveu hoje');
+    });
+
+
+
     return router;
   }
+
 }
