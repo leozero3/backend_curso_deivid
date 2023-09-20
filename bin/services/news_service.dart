2fa1690f5e8fd5bab6682646f17af3c1d0969c03
Leo Home <realleo2009@hotmail.com>
2023-09-19 23:16:47 -0300

    #13 12:40

--- bin/services/news_service.dart
+++ bin/services/news_service.dart
@@ -1,8 +1,9 @@
 import '../model/news_model.dart';
 import 'generic_service.dart';
+import '../utils/list_extension.dart';
 
 class NewsService implements GenericService<NewsModel> {
-  List<NewsModel> _fakeDB = [];
+  final List<NewsModel> _fakeDB = [];
 
   @override
   bool delete(int id) {
@@ -22,12 +23,17 @@ class NewsService implements GenericService<NewsModel> {
 
   @override
   bool save(NewsModel value) {
-    _fakeDB.firstWhere(
+    NewsModel? model = _fakeDB.firstWhereOrNull(
       (e) => e.id == value.id,
-      orElse: () => value,
     );
 
-    // _fakeDB.
+    if (model == null) { //se NULO, novo objeto
+      _fakeDB.add(value);
+
+    }else{
+      var index =_fakeDB.indexOf(model); // se nao NULO, atualiza o objeto
+      _fakeDB[index] = value;
+    }
     return true;
   }
 }
--- bin/utils/list_extension.dart
+++ bin/utils/list_extension.dart
@@ -1,3 +1,8 @@
-extension ListExtension<E> on List<E>{
-
+extension ListExtension<E> on List<E> {
+  E? firstWhereOrNull(bool Function(E element) test) {
+    for (E element in this) {
+      if (test(element)) return element;
+    }
+    return null;
+  }
 }
