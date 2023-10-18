89f89b7fe69c22eceb917faf657786ce31f63987
leo work <realleo2009@hotmail.com>
2023-10-18 15:28:45 -0300

    #20

--- bin/backend.dart
+++ bin/backend.dart
@@ -3,6 +3,7 @@ import 'api/news_api.dart';
 import 'api/login_api.dart';
 import 'infra/custom_server.dart';
 import 'infra/middleware_interception.dart';
+import 'infra/security/security_service.dart';
 import 'infra/security/security_service_imp.dart';
 import 'services/news_service.dart';
 import 'utils/custom_env.dart';
@@ -10,16 +11,18 @@ import 'utils/custom_env.dart';
 void main() async {
   CustomEnv.fromFile('.env-dev');
 
+  SecurityService _securityService = SecurityServiceImp();
+
   var cascadeHandler = Cascade()
-      .add(LoginApi(SecurityServiceImp()).handler)
+      .add(LoginApi(_securityService).handler)
       .add(NewsApi(NewsService()).handler)
       .handler;
 
   var handler = Pipeline()
       .addMiddleware(logRequests())
       .addMiddleware(MiddlewareInterception().middleware)
-      .addMiddleware(SecurityServiceImp().authorization)
-      .addMiddleware(SecurityServiceImp().verifyJwt)
+      .addMiddleware(_securityService.authorization)
+      .addMiddleware(_securityService.verifyJwt)
       .addHandler(cascadeHandler);
 
   await CustomServer().initialize(
